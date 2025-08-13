f8bf8615af1aa51167cf927682637fc6f09932b0
CarolinaPinheiroSantos <pinheirocarol0506@gmail.com>
2025-08-13 16:20:25 -0300

    13/08/25

---
+++ Exceções/aula.dart
@@ -0,0 +1,16 @@
+import 'dart:io';
+
+//vazio é nao ter nada e null é quando tem a estrutura
+
+// .trim() -> ignora espaços vazios
+// .isEmpty() -> é vazio
+
+//Try -> tente isso 
+//catch -> pegue o erre na variavel 'e'
+//on -> especificar o erro
+// finally -> idenpedente do que aconteça mostra
+
+
+void main(){
+
+}
